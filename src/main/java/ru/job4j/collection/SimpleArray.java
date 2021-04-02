f4fe03fe9aa9ee3fcc222432928fb3f6d5450e0c
Stilan <40999153+Stilan@users.noreply.github.com>
2021-04-02 10:13:33 +0700

    1. Динамический список на массиве. [#490417]

--- src/main/java/ru/job4j/collection/SimpleArray.java
+++ src/main/java/ru/job4j/collection/SimpleArray.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
-public class SimpleArray<T> implements Iterable<T> {
+public class SimpleArray<T> implements Iterable<T>{
     private Object[] container = new Object[10];
     private int size;
     int modCount = 0;
