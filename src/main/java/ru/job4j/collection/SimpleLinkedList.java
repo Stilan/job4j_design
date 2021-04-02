e81bee7e597159562bdcca95906cf274c865ac8d
Stilan <40999153+Stilan@users.noreply.github.com>
2021-04-02 19:10:41 +0700

    1. Динамический список на массиве. [#490417]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,75 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements List<E> {
+
+    private int size = 0;
+    private Node<E> first;
+    private Node<E> last;
+    private int modCount = 0;
+    @Override
+    public void add(E value) {
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> j = first;
+        for (int i = 0; i < index; i++) {
+            j = j.next;
+        }
+        return j.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        int expectedModCount = modCount;
+        return new Iterator<E>() {
+            int i = 0;
+            @Override
+            public boolean hasNext() {
+                return i < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                Node<E> iteratorNode = first;
+                for (int j = 0; j < i; j++) {
+                    iteratorNode = first.next;
+                }
+                i++;
+               return iteratorNode.item;
+
+            }
+        };
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
