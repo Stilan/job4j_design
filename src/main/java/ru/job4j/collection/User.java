4693f0c8697b0b6a2e8551c69529bead9a809e01
Stilan <40999153+Stilan@users.noreply.github.com>
2021-04-10 17:15:33 +0700

    1. Создать модель User [#490444]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -10,6 +10,6 @@ public class User {
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
-        this.birthday = birthday;
+        this.birthday = birthday ;
     }
 }
