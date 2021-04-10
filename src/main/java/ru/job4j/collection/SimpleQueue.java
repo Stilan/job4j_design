5b9a4f2243ed663dcc44c1ce6a2d7d829a359bcc
Stilan <40999153+Stilan@users.noreply.github.com>
2021-04-10 23:59:50 +0700

    1. Реализовать коллекцию Set на массиве [#490441]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -2,7 +2,7 @@ package ru.job4j.collection;
 
 public class SimpleQueue<T>{
     private final SimpleStack<T> in = new SimpleStack<>();
-    private final SimpleStack<T> out = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>() ;
 
     public T poll() {
         if (out.isEmpty()) {
