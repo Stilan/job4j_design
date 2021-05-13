52ec0773d5f5bad1a29e6b2e081cabc2696ab00b
Stilan <40999153+Stilan@users.noreply.github.com>
2021-05-13 16:19:13 +0700

    0.4. BufferedOutputStream [#252490 #55777]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,23 +1,25 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(new FileOutputStream("result.txt")))) {
             int[][] table = multiple(10);
             for (int i = 0; i < table.length; i++) {
                 for (int j = 0; j < table.length; j++) {
-                      out.write((table[i][j] + "\t").getBytes());
+                      out.write(String.valueOf(table[i][j]) + "\t");
                 }
-                out.write("\n".getBytes());
+                out.write("\n");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println(Arrays.deepToString(multiple(10)));
     }
     public static int[][] multiple(int size) {
         int[][] table = new int[size][size];
