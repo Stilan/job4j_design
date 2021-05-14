ff2776cc6362b58149946b01ede8b60dbc9e057c
Stilan <40999153+Stilan@users.noreply.github.com>
2021-05-14 10:55:46 +0700

    1. Читаем файл конфигурации [#490470]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            for (String line = read.readLine(); line != null; line = read.readLine()) {
+                if (line.contains("#")) {
+                    continue;
+                }
+               String[] str1 = line.split("=");
+               values.put(str1[0], str1[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+      //  throw new UnsupportedOperationException("Don't impl this method yet!");
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
