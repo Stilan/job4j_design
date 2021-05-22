4ddfc16dae22042a3250d903063f87e1b530627a
Stilan <40999153+Stilan@users.noreply.github.com>
2021-05-22 14:18:38 +0700

    Валидация - это процесс проверки данных перед выполнением операции.

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -5,7 +5,11 @@ import java.io.File;
 public class Dir {
 
     public static void main(String[] args) {
-        File file = new File("/Users/aleksandrlitvinov/projects/job4j_design");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
+//      File file = new File("/Users/aleksandrlitvinov/projects/job4j_design");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
