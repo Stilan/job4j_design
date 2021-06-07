710db3ec37a92a3dde4324e831660b94fcefdd49
Stilan <aleksandr.litvinov89qw@gmail.com>
2021-06-07 13:02:10 +0700

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,13 +13,21 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String str2 = in.readLine();
+                    str2 = str2.split("=")[1];
+                    str2 = str2.split(" ")[0];
                     while (!(str = in.readLine()).isEmpty()) {
-                        if (str.contains("Bye")) {
-                            server.close();
-                        }
                         System.out.println(str);
                     }
+                    if (str2.equals("Hello")) {
                         out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write("Hello".getBytes());
+                    } else if (str2.equals("Exit")) {
+                        server.close();
+                    } else {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write("What".getBytes());
+                    }
                 }
             }
         }
