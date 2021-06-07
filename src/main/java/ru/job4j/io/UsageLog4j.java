ecb0dd97115aefa638ffee9dcd35ebeb74969712
Stilan <40999153+Stilan@users.noreply.github.com>
2021-06-07 15:25:52 +0700

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Иван";
+        String surname = "Иванов";
+        String position = "Студент";
+        int course = 2;
+        String specialty = "Программист";
+        double averageScore = 4.1;
+        long gradeBookNumber = 234565651;
+        int age = 31;
+
+        LOG.debug("имя :{} \n фамилия :{}\n профессия :{}\n курс :{}\n специальность :{}\n "
+                        + "средний балл :{}\n номер зачетки :{}\n возрост :{}", name, surname, position, course,
+                        specialty, averageScore, gradeBookNumber, age);
     }
 }
