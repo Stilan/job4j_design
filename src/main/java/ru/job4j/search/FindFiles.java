d14bb11c2dd55702899eb0de5db8ea791456a14b
Stilan <40999153+Stilan@users.noreply.github.com>
2021-07-02 09:18:52 +0700

    2. Поиск файлов по критерию [#783 #173058]

--- src/main/java/ru/job4j/search/FindFiles.java
+++ src/main/java/ru/job4j/search/FindFiles.java
@@ -17,12 +17,12 @@ public class FindFiles {
 
     public static Find filter(Args args) {
         Find find = null;
-        if (args.getType().equals("mask")) {
-            find = new Find(path -> path.toFile().getName().endsWith(args.getName()));
-        } else if (args.getType().equals("name")) {
-            find = new Find(path -> path.toFile().getName().equals(args.getName()));
-        } else if (args.getType().equals("regex")) {
-            find = new Find(path -> path.toFile().getName().startsWith(args.getName()));
+        if (args.getMapArgs().get("t").equals("mask")) {
+            find = new Find(path -> path.toFile().getName().endsWith(args.getMapArgs().get("n")));
+        } else if (args.getMapArgs().get("t").equals("name")) {
+            find = new Find(path -> path.toFile().getName().equals(args.getMapArgs().get("n")));
+        } else if (args.getMapArgs().get("t").equals("regex")) {
+            find = new Find(path -> path.toFile().getName().startsWith(args.getMapArgs().get("n")));
         }
         return find;
     }
@@ -30,7 +30,8 @@ public class FindFiles {
     public static void write(String output, List<Path> paths) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(output))) {
             for (Path path : paths) {
-                bw.write(String.valueOf(path + System.lineSeparator()));
+                bw.write(String.valueOf(path));
+                bw.newLine();
                 bw.flush();
             }
         } catch (IOException ex) {
@@ -39,9 +40,9 @@ public class FindFiles {
     }
 
     public static void main(String[] args) throws IOException {
-      //  Args arg = new Args(new String[]{"-d=/Users/aleksandrlitvinov/Desktop/TestJob4j-n=Без названия.txt-t=name-o=log.txt"});
-        Args arg = new Args(args);
-        List<Path> result = search(Paths.get(arg.getDirectory()), arg);
-        write(arg.getOutput(), result);
+        Args arg = new Args(new String[]{"-d=/Users/aleksandrlitvinov/Desktop/TestJob4j-n=Без названия.txt-t=name-o=log.txt"});
+    //    Args arg = new Args(args);
+        List<Path> result = search(Paths.get(arg.getMapArgs().get("d")), arg);
+        write(arg.getMapArgs().get("o"), result);
     }
 }
